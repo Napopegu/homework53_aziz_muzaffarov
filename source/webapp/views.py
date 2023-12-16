4567afb1293031d6e3451d771a40a71337988548
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-16 19:39:16 +0600

    Создал представление index_view

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
-# Create your views here.
+from webapp.models import Task
+def index_view(request):
+    tasks = Task.objects.all()
+    return render(request, template_name='index.html', context={'tasks': tasks})
