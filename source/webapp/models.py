9a25fd636455fef667c0c448524805c2cb5f30b7
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-16 20:13:46 +0600

    Создание файла index.html и проведение пути к нему.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -4,6 +4,9 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 class Task(models.Model):
     description = models.CharField(max_length=60, null=False, blank=False, verbose_name="Описание")
     status = models.CharField(max_length=20, choices=status_choices, default=status_choices[0][0],verbose_name="Статус")
-    date_of_finish = models.DateField(verbose_name='Дата выполнения')
+    date_of_finish = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f'{self.pk}.{self.description}'
