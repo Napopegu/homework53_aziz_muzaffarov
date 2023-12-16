8ceeaee827de29eaf1e6d2dd1e8c17303ac4821f
Napopegu <142897234+Napopegu@users.noreply.github.com>
2023-12-17 01:34:14 +0600

    корректировки файлов html

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from webapp.models import Task, status_choices
 from django.http import HttpResponseRedirect
 
